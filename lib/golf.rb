da634b8bf07b9d84abacfdd5f2d43a6b671072de
Christopher de Bruin <christopher.debruin@mindmatters.de>
2011-07-20 09:43:20 +0200

    some minor improvement

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,27 +1,24 @@
 class Golf
-  class << self
-    x = 0
-    [
-    'p.inject :*',
-    '(p[0]..p[1]).select{|n| n.to_s == n.to_s.reverse}',
-    'a = p.downcase
-      a == a.reverse',
-    %q{p.map { |t| t.sub(/(man.*)/,'hat(\1)').sub(/(dog.*)\)/,'\1(bone))').sub(/cat/,'dead') }},
-    '(1..4).map{|i| p.each_cons(i).to_a}.inject :+',
-    '(1..p).map{ |i|
-        s="fizz" if i % 3 == 0
-        s="#{s}buzz" if i % 5 == 0
-        s || i
-      }',
-    "i = -1
-      m = []
-      p.map{ |j| p[i] + 1 != p[i += 1] ? m << [j] : (m[-1] << j if p[i] + 1 != p[i + 1]) }
-      m.map{ |i| i * '-' }",
-    "b = [1,1]
-      (2..p-1).map {|i| b << b[-2] + b[-1] }
-      b"
-    ].map {|m| eval "def hole#{x += 1} p
-    #{m}
-    end"}
-  end
+  x = 0
+  [
+  'p.inject :*',
+  '(p[0]..p[1]).select{|n| n.to_s == n.to_s.reverse}',
+  'p.downcase! == p.reverse',
+  %q{p.map { |t| t.sub(/(man.*)/,'hat(\1)').sub(/(dog.*)\)/,'\1(bone))').sub(/cat/,'dead') }},
+  '(1..4).map{|i| p.each_cons(i).to_a}.inject :+',
+  '(1..p).map{ |i|
+      s="fizz" if i % 3 == 0
+      s="#{s}buzz" if i % 5 == 0
+      s || i
+    }',
+  "i = -1
+    m = []
+    p.map{ |j| p[i] + 1 != p[i += 1] ? m << [j] : (m[-1] << j if p[i] + 1 != p[i + 1]) }
+    m.map{ |i| i * '-' }",
+  "b = [1,1]
+    (2..p-1).map {|i| b << b[-2] + b[-1] }
+    b"
+  ].map {|m| eval "def self.hole#{x += 1} p
+  #{m}
+  end"}
 end
